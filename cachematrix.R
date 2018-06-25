490a1b49e76b7abcc8a0fcb51430565f18acf5a4
ayush-gupta168 <40562276+ayush-gupta168@users.noreply.github.com>
2018-06-25 15:16:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,22 @@
+## Put comments here that give an overall description of what your
+## functions do
+## Created By:  Ayush Gupta
+## Created On: June 25, 2018
+## These functions facilitate calculate the inverse of a matrix.  To make
+## the calculation more efficient, cacheSolve() will look for a cached inverse
+## solution before calculating the inverse again.  mackCacheMatrix() provides
+## the functionality to store and retrieve a cached inverse solution to be
+## retrieved later.
+ 
+## Write a short comment describing this function
+## makeCacheMatrix returns a list with four funtions to use to cache a matrix
+## and its inverse.
+## set() assigns the matrix.  
+## get() returns the matrix.
+## getInv() returns the inverse.
+## setInv() assigns the matrix inverse.
+
 ## A pair of functions that cache the inverse of a matrix
-
-
 ## Creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function( m = matrix() ) {
 
@@ -63,3 +79,24 @@ cacheSolve <- function(x, ...) {
     ## Return the matrix
     m
 }
+## testing code 
+## matrix <- rbind(c(0, -3, -2), c(1, -4, -2), c(-3, 4, 1))
+## inverse <- solve(matrix)
+##my_matrix <- makeCacheMatrix(matrix)
+##my_matrix$getInv()
+## my_inverse <- cacheSolve(my_matrix)
+## my_inverse
+## my_inverse <- cacheSolve(my_matrix)
+## my_inverse
+## my_matrix$getInv() 
+## matrix2 <- rbind(c(1, 2), c(1, 1))
+## inverse2 <- solve(matrix2)
+## my_matrix2 <- makeCacheMatrix(matrix2)
+## my_matrix2$getInv()
+## my_inverse2 <- cacheSolve(my_matrix2)
+## my_inverse2
+## my_inverse2 <- cacheSolve(my_matrix2)
+## my_inverse2
+## my_matrix2$getInv()
+## my_matrix$getInv()
+## my_matrix2$getInv()
